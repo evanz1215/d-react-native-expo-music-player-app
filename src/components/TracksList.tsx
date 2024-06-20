4e471d0dcad01f2b4f054d22e4b6d1b98e5f08f8
Evan <evanz1215@gmail.com>
2024-06-20 23:34:58 +0800

    TracksList  empty

---
+++ src/components/TracksList.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const TracksList = () => {
+	return (
+		<View>
+			<Text>TracksList</Text>
+		</View>
+	)
+}
+
+export default TracksList
---
+++ src/components/TracksListItem.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const TracksListItem = () => {
+	return (
+		<View>
+			<Text>TracksListItem</Text>
+		</View>
+	)
+}
+
+export default TracksListItem
